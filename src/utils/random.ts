80d2cc5f8cac228b1b5b5e63bba6106d157e144d
D4rkr34lm <manuel.frohn.09.12@gmail.com>
2026-05-20 17:03:42 +0200

    remove unused random helper

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -19,9 +19,3 @@ export function getUniqueRandomArray({
 
   return Array.from(randoms);
 }
-
-export function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
