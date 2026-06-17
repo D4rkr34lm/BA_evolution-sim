85567343611d692f9b621099efd10932b68ba31f
D4rkr34lm <manuel.frohn.09.12@gmail.com>
2026-06-17 15:18:09 +0200

    add chart js and lit wrapper

---
+++ src/widgets/chart-wrapper.ts
@@ -0,0 +1,110 @@
+import { css, PropertyValues } from "lit";
+import { LitElementWw } from "@webwriter/lit";
+import { customElement, property, query } from "lit/decorators.js";
+import { html } from "@lit-labs/signals";
+import {
+  Chart,
+  ChartConfiguration,
+  ChartData,
+  ChartOptions,
+  ChartType,
+  registerables,
+} from "chart.js";
+import { hasNoValue } from "@/utils/typeGuards";
+
+Chart.register(...registerables);
+
+@customElement("chart-wrapper")
+export class ChartWrapper extends LitElementWw {
+  @property({ attribute: true })
+  accessor type: ChartType = "line";
+
+  @property({ attribute: false })
+  accessor data: ChartData = { datasets: [] };
+
+  @property({ attribute: false })
+  accessor chartOptions: ChartOptions = {};
+
+  @property({ attribute: true })
+  accessor label = "Chart";
+
+  @query("canvas")
+  accessor canvas!: HTMLCanvasElement;
+
+  private chart: Chart | null = null;
+
+  static readonly styles = css`
+    :host {
+      display: block;
+      min-height: 18rem;
+    }
+
+    .canvas-frame {
+      height: 18rem;
+      position: relative;
+      width: 100%;
+    }
+
+    canvas {
+      height: 100% !important;
+      width: 100%;
+    }
+  `;
+
+  private createChart() {
+    const context = this.canvas.getContext("2d");
+
+    if (hasNoValue(context)) {
+      throw new Error("Could not create chart without a 2D canvas context");
+    }
+
+    this.chart = new Chart(context, {
+      type: this.type,
+      data: this.data,
+      options: this.chartOptions,
+    } as ChartConfiguration);
+  }
+
+  private updateChart() {
+    if (hasNoValue(this.chart)) {
+      return;
+    }
+
+    this.chart.data = this.data;
+    this.chart.options = this.chartOptions;
+    this.chart.update("none");
+  }
+
+  protected firstUpdated() {
+    this.createChart();
+  }
+
+  protected updated(changedProperties: PropertyValues<this>) {
+    if (hasNoValue(this.chart)) {
+      return;
+    }
+
+    if (changedProperties.has("type")) {
+      this.chart.destroy();
+      this.createChart();
+      return;
+    }
+
+    this.updateChart();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.chart?.destroy();
+    this.chart = null;
+  }
+
+  render() {
+    return html`
+      <div class="canvas-frame">
+        <canvas aria-label=${this.label} role="img"></canvas>
+      </div>
+    `;
+  }
+}
